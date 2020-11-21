aa9cb6241bc4c753552ddc64a3325347bdaf4e48
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-11-21 09:17:59 -0300

    Update gpt-2.py

--- gpt-2.py
+++ gpt-2.py
@@ -1,7 +1,6 @@
 import warnings
 warnings.filterwarnings("ignore")
 
-import pandas as pd
 import streamlit as st
 from transformers import pipeline, set_seed
 from transformers.pipelines import TextGenerationPipeline
@@ -9,10 +8,8 @@ from transformers.pipelines import TextGenerationPipeline
 from googletrans import Translator
 translator = Translator()
 
-
 class TextGenerator:
     def __init__(self):
-        df.dropna() # aletração 
         self.generator: TextGenerationPipeline
         self.max_length = 300
         set_seed(1)
@@ -25,7 +22,6 @@ class TextGenerator:
                               max_length=self.max_length,
                               num_return_sequences=1)[0]['generated_text']
 
-
 @st.cache(allow_output_mutation=True)
 def instantiate_generator():
     generator = TextGenerator()
@@ -36,12 +32,8 @@ if __name__ == '__main__':
     st.title('GPT-2')
 
     starting_text = st.text_area('Deixe o GPT-2 completar sua frase..') 
-
     generator = translator.translate(starting_text, src= 'pt',dest='en')							
-
     generator = instantiate_generator()
-
-   
 
     if starting_text:
         response = generator.generate_text(starting_text)
