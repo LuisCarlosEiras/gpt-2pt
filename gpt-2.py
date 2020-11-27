25e7bb394d07a161ef85f70c4da4472b75e30ef6
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-11-27 16:12:45 -0300

    Update gpt-2.py

--- gpt-2.py
+++ gpt-2.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
         response = generator.generate_text(text_unlim)
         result = translator.translate(response, src= 'en',dest='pt')
         
-        result = ("AttributeError: 'NoneType' object has no attribute 'group'")
+    if result = ("AttributeError: 'NoneType' object has no attribute 'group'")
         return text_unlim
         
         st.markdown(f'Completed phrase: {result}')
