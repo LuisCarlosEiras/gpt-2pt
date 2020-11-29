24923d89022dcce712ab83f866c43bdd67c9c4f7
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-11-29 11:40:16 -0300

    Update gpt-2.py

--- gpt-2.py
+++ gpt-2.py
@@ -37,6 +37,8 @@ if __name__ == '__main__':
     text_unlim = st.text_area("Escreva suas palavras ou frases abaixo", "Escreva aqui e clique Ctrl + Enter")
     generator = translator.translate(text_unlim, src='pt', dest='en')							
     generator = instantiate_generator()
+    instantiate_generator = ("AttributeError: 'NoneType' object has no attribute 'group'")
+    retunr generator
    
 
     if text_unlim:
