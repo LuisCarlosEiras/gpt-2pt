c005f5c6600e7d03207066ea4a2e4bec121d343e
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-12-02 07:17:45 -0300

    Update gpt-2.py

--- gpt-2.py
+++ gpt-2.py
@@ -37,13 +37,15 @@ if __name__ == '__main__':
     st.title('GPT-2 em português')
 
     text_unlim = st.text_area("Escreva suas palavras ou frases abaixo", "Escreva aqui e clique Ctrl + Enter")
-    # generator = translator.translate(text_unlim, src='pt', dest='en')	
-    generator = translator.translate(text_unlim, to_lang="en") # src='pt', dest='en')	
+    translator = Translator(to_lang = "en")
+    generator = translator.translate(text_unlim)# , src='pt', dest='en')	
+    # generator = translator.translate(text_unlim, to_lang="en") # src='pt', dest='en')	
     generator = instantiate_generator()
      
     if text_unlim:
         response = generator.generate_text(text_unlim)
-        result = translator.translate(response, to_lang="pt") # src= 'en', dest='pt')               
+        translator = Translator(to_lang = "pt")
+        result = translator.translate(response) # , to_lang="pt") # src= 'en', dest='pt')               
         st.markdown(f'Completed phrase: {result}')            
 
     
