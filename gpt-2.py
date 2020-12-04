83ed2f20a93a6c907fab2894de2f44c45bf2a01d
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-12-04 18:16:07 -0300

    Update gpt-2.py

--- gpt-2.py
+++ gpt-2.py
@@ -11,14 +11,10 @@ from transformers.pipelines import TextGenerationPipeline
 from google_trans_new import google_translator 
 translator = google_translator()
 
-translate_text = translator.translate(p.text, lang_tgt='pt')  
-st.write(translate_text)
-st.write("""*Fonte*: Netcraft""")
-
 class TextGenerator:
     def __init__(self):
         self.generator: TextGenerationPipeline
-        self.max_length = 50
+        self.max_length = 500
         set_seed(1)
 
     def load_generator(self) -> None:  
@@ -39,12 +35,12 @@ if __name__ == '__main__':
     st.title('GPT-2 em português')
 
     text_unlim = st.text_area("Escreva suas palavras ou frases abaixo", "Escreva aqui e clique Ctrl + Enter")
-    translate_text = translator.translate(text_unlim,  lang_tgt='pt')  
+    translate_text = translator.translate(text_unlim, lang_tgt='en')  
     generator = translate_text()
      
     if text_unlim:
         response = generator.generate_text(text_unlim)
-        translator = Translator(to_lang = "pt")
+        translator = Translator(to_lang = 'pt')
         result = translator.translate(response)                
         st.markdown(f'Completed phrase: {result}')            
 
