a2dc9536e383aeaf57eda2f1cfccd93438ff8db0
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-12-04 18:29:57 -0300

    Update gpt-2.py

--- gpt-2.py
+++ gpt-2.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
      
     if text_unlim:
         response = generator.generate_text(text_unlim)
-        translator = Translator(to_lang = 'pt')
+        translate_text = translator.translate(text_unlim, lang_tgt='pt')  
         result = translator.translate(response)                
         st.markdown(f'Completed phrase: {result}')            
 
