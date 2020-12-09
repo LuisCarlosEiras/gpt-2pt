5ac0f4d578f068d47d06d70db5b5e51069f397dd
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2020-12-09 08:10:36 -0300

    Update gpt-2.py
    open gtp-2

--- gpt-2.py
+++ gpt-2.py
@@ -15,7 +15,7 @@ class TextGenerator:
         set_seed(1)
 
     def load_generator(self) -> None:
-        self.generator = pipeline('text-generation', model='gpt2')
+        self.generator = pipeline('text-generation', model='opengpt-2')
 
     def generate_text(self, starting_text: str) -> str:
         return self.generator(starting_text,
